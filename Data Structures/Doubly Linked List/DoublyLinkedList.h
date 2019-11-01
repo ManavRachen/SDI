8aee15d0f30dc47928ef0dcfcbe497ea3513dd86
Bradley Smith <56271540+N0822325@users.noreply.github.com>
2019-11-01 04:33:35 +0000

    Add files via upload

---
+++ Data Structures/Doubly Linked List/DoublyLinkedList.h
@@ -0,0 +1,30 @@
+#pragma once
+
+class DoublyLinkedList
+{
+public:
+	DoublyLinkedList();
+	~DoublyLinkedList();
+	DoubleNode* operator[](int pos); //Overloads the [] operator ~ calls Search()
+
+	void append(int data); //Add node at end of list
+	void prepend(int data); //Add node at start of list
+	void insert(int data, int pos); //Add node at index x
+	void popBack(); //Delete node at end of list
+	void popFront(); //Delete node at start of list
+	void del(int pos); //Delete node at index x
+
+	DoubleNode* at(int pos); //Return node at index x
+	DoubleNode* front(); //Return first node
+	DoubleNode* back(); //Return final node
+
+	void populate(unsigned int size); //Add x amount of random nodes to end of list
+
+	unsigned int size();
+	void print();
+
+private:
+	DoubleNode* head;
+	DoubleNode* tail;
+	void OutOfBounds(int pos); //Checks if input is in range
+};
