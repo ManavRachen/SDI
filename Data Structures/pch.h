596babb11d8aa79fc61076477e54024e77244699
Bradley Smith <N0822325@my.ntu.ac.uk>
2019-11-01 17:54:33 +0000

    Adding Queue

--- Data Structures/pch.h
+++ Data Structures/pch.h
@@ -3,14 +3,15 @@
 
 #include "Nodes\Node.h"
 #include "Nodes\DoubleNode.h"
-#include "Nodes\template Node.h"
+#include "Nodes\Template Node.h"
 #include "Nodes\Template DoubleNode.h"
 
 #include "Simple Structures\Stack.h"
+#include "Simple Structures\Queue.h"
 
 #include "Singly Linked List\LinkedList.h"
 #include "Doubly Linked List\DoublyLinkedList.h"
-#include "Singly Linked List\Template List\template Linked List.h"
+#include "Singly Linked List\Template List\Template Linked List.h"
 #include "Doubly Linked List\Template DoubleList\Template DoublyLinkedList.h"
 
 #include "Binary Search Tree\BinarySearchTree.h"
