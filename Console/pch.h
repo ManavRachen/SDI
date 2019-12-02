8daf83f408c73da6776d8bdb0232344302427136
Bradley Smith <N0822325@my.ntu.ac.uk>
2019-12-02 20:10:07 +0000

    git commit

--- Console/pch.h
+++ Console/pch.h
@@ -1,14 +1,19 @@
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+#pragma once
 
-#ifndef PCH_H
 #define PCH_H
 
-// TODO: add headers that you want to pre-compile here
+// Data Structures
+#include "../Data Structures/Nodes/Node.h"
+#include "../Data Structures/Nodes/DoubleNode.h"
+#include "../Data Structures/Nodes/Template Node.h"
+#include "../Data Structures/Nodes/Template DoubleNode.h"
+#include "../Data Structures/Simple Structures/Stack.h"
+#include "../Data Structures/Simple Structures/Queue.h"
+#include "../Data Structures/Singly Linked List/LinkedList.h"
+#include "../Data Structures/Doubly Linked List/DoublyLinkedList.h"
+#include "../Data Structures/Singly Linked List/Template List/Template Linked List.h"
+#include "../Data Structures/Doubly Linked List/Template DoubleList/Template DoublyLinkedList.h"
+#include "../Data Structures/Binary Search Tree/BinarySearchTree.h"
 
-#endif //PCH_H
+
+//
