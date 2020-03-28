1ca0d15046b3eee4944b427d9a03fc9d4662f8c1
Bradley Smith <N0822325@my.ntu.ac.uk>
2020-03-28 05:26:19 +0000

    delting bs files

--- Other/lab_6_recursion_STL/Source.cpp
+++
@@ -1,13 +0,0 @@
-#include <iostream>
-using namespace std;
-
-
-int recEx2(int x) {
-	if (x <= 0)
-		return 0;
-	else if (x == 1)
-		return 2;
-	else
-		return (recEx2(x - 1) + recEx2(x - 2) + recEx2(x - 3));
-}
-void main() {	cout << recEx2(9) << endl;}
